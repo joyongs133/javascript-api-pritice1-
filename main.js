5ea6e7493210e4e18a703d73392c89d5712cff00
joyongs133 <joyong133@naver.com>
2022-11-24 19:49:19 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const getMovies = movieName => {
+   return new Promise((resolve, reject) => {
+      fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+      .then(res => res.json())
+      .then(json => {
+         console.log(json)
+         if (json.Response === 'False') {
+            reject(json.Error)
+         }
+         resolve(json)
+      })
+      .catch(error => {
+         reject(error)
+      })
+   })
+}
+
+let loading = true
+
+getMovies('avengers')
+  .then(movies => console.log('영화 목록:', movies))
+  .catch(error => console.log('에러 발생:', error))
+  .finally(() => loading = false)
+
+  //async/ await
+
+  const wrap = async () => {
+  try {
+   const movies = await getMovies('avengers')
+   console.log('영화 목록:', movies)
+  } catch (error) {
+   console.log('에러 발생:', error)
+  } finally {
+   loading = false
+  }
+  }
+
+  wrap()
